ad95dc4b4f60c52cb4e9acf865377e5a40efe4c2
lsphantom <gp@limestripes.us>
2020-06-08 21:49:36 -0600

    add some comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 //JS programmed for UCAR 2020 by Gary Pacheco
+
+//vars
 var openCredits = document.getElementById('credits-btn');
 var closeCredits = document.getElementById('close-btn');
 var credits = document.getElementById('credits');
 
+
+//toggle credits
 function toggleCreditsModal (action) {
     if (action === 'open') {
         credits.style.display = 'block';
@@ -11,3 +15,5 @@ function toggleCreditsModal (action) {
     }
     
 }
+
+//climate sensitivity calculator
