e29997558b69a25f83e068315cafbe3403e2748a
lsphantom <gp@limestripes.us>
2020-07-07 12:16:22 -0600

    add basic read and writes on load

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 //JS programmed for UCAR 2020 by Gary Pacheco
-
-//vars
+//global vars
 var openCredits = document.getElementById('credits-btn');
 var closeCredits = document.getElementById('close-btn');
 var credits = document.getElementById('credits');
@@ -16,4 +15,66 @@ function toggleCreditsModal (action) {
     
 }
 
-//climate sensitivity calculator
+
+//JQUERY
+$(document).ready(function(){
+    var climateSensitivity = $('input[name="sensitivity"]:checked').val();
+    var baselineTemp = 0;
+    var b1 = [390, 14.6];
+    var b2 = [370, 14.3];
+    var b3 = [280, 13.7];
+    var b4 = [182, 5];
+
+
+    //On load setup co2 concentration and AGT
+    function checkSelectedValues(){
+       switch ($('#baseline').val()) {
+        case 'b1':
+        climateSensitivity = b1[0];
+        baselineTemp = b1[1];
+        break;
+
+        case 'b2':
+        climateSensitivity = b2[0];
+        baselineTemp = b2[1];
+        break;
+
+        case 'b3':
+        climateSensitivity = b3[0];
+        baselineTemp = b3[1];
+        break;
+
+        case 'b4':
+        climateSensitivity = b4[0];
+        baselineTemp = b4[1];
+        break;
+
+        default:
+        break;
+        } 
+    }
+    
+
+    //Set outputs to selected values
+    function setValues() {
+       checkSelectedValues();
+       $('#ppmv-range').val(climateSensitivity);
+       $('#agt').text(baselineTemp); 
+    }
+
+    setValues();
+    
+
+
+    //Dropdown change
+    $('#baseline').on('change', function(){
+        setValues();
+    });
+
+
+//setup range selector
+//$('#agt').value(baseline).data('temp');
+
+
+
+});
